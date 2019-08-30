79e8acdc1b9a5910097010b0a8dcfe0eb8947de4
Eliza Weisman <eliza@buoyant.io>
2019-08-30 14:06:51 -0700

    core: rename internal API
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tracing/src/macros.rs
+++ tracing/src/macros.rs
@@ -2065,7 +2065,7 @@ macro_rules! callsite {
 #[doc(hidden)]
 macro_rules! level_enabled {
     ($lvl:expr) => {
-        $crate::dispatcher::exists() && $lvl <= $crate::level_filters::STATIC_MAX_LEVEL
+        $crate::dispatcher::has_been_set() && $lvl <= $crate::level_filters::STATIC_MAX_LEVEL
     };
 }
 
